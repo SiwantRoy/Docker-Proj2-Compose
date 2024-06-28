e5bee3582871eaf2fd629358874b9bbcc76bda12
siwant <siwantroy0612@gmail.com>
2024-06-28 15:27:07 -0700

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('Message received: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// frontend/src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Chat Application</h1>
+      {/* Your chat components go here */}
+    </div>
+  );
+}
+
+export default App;
